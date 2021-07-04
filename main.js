9c27f48bab4d4bf6c2f0fc18cc09e0e1e7ecb3d1
Nguyen Ngoc Sang <ngocsang1201@gmail.com>
2021-07-04 20:02:29 +0700

    add files

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('game-zone')
+const context = canvas.getContext('2d')
+const scoreShow = document.getElementById('score')
+
+const birdImg = new Image()
+const background = new Image()
+const 
